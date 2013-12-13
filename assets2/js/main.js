df75c166b64c94402c063575d7c66d774336d18f
Spencer Alderman <Spencer@129.21.140.157>
2013-12-13 01:38:56 -0500

    Begin updating to bootstrap 3

---
+++ assets2/js/main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+
+    var upload = 0;
+
+    $(function() {
+        var addfile = function() {
+            var newRow = $("<tr><td class=\"field_name span4\"><strong></strong></td>"
+                    + "<td class=\"field_option\"><input type=\"file\" class=\"file" + (upload + 1).toString() + "\" name=\"pictures\">")
+                    .insertAfter($(this).closest('tr'));
+            newRow.find('input.file' + (upload + 1).toString()).change(addfile);
+            $("input.file" + upload.toString()).off('change');
+            upload++;
+        };
+        $("input.file" + upload.toString()).change(addfile);
+    });
+});
